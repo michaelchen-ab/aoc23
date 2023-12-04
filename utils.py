07ee949f536366f9137a5a261b5ab0a2b644d2c8
Michael Chen <michael.chen@applyboard.com>
2023-12-04 00:26:39 -0500

    easier way to get simple input

--- utils.py
+++ utils.py
@@ -2,11 +2,11 @@ import os
 import pathlib
 
 
-def get_input_file(day):
+def get_input_file(day: int | str):
     return (pathlib.Path(__file__).parent / "inputs" / str(day)).resolve()
 
 
-def get_input(day):
+def get_input(day: int | str):
     path = get_input_file(day)
     if not os.path.isfile(path):
         raise FileNotFoundError(f"https://adventofcode.com/2023/day/{day}/input")
