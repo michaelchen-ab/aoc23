c5cac5ea9a01e3fa67b753d89a14857cfca54a3a
Michael Chen <michael.chen@applyboard.com>
2023-12-04 00:22:09 -0500

    nicer way to split by arbitrary whitespace, tidy up part 2 summing

--- p4.py
+++ p4.py
@@ -6,8 +6,8 @@ total = 0
 for line in data:
     _, nums = line.split(": ")
     win, mine = nums.split(" | ")
-    win = [n for n in win.split(" ") if n]
-    mine = [n for n in mine.split(" ") if n]
+    win = win.split()
+    mine = mine.split()
     score = 0
     for num in win:
         if num in mine:
@@ -15,13 +15,13 @@ for line in data:
     total += score
 print(total)
 
-arr = [1] * (len(data) + 1)
+arr = [0] + [1] * len(data)
 for idx, line in enumerate(data, start=1):
     _, nums = line.split(": ")
     win, mine = nums.split(" | ")
-    win = [n for n in win.split(" ") if n]
-    mine = [n for n in mine.split(" ") if n]
+    win = win.split()
+    mine = mine.split()
     c = sum(1 for n in win if n in mine)
     for i in range(idx + 1, idx + c + 1):
         arr[i] += arr[idx]
-print(sum(arr) - 1)
+print(sum(arr))
