58b49a431d470b46a67634431fd6d07998a69dd1
Michael Chen <michael.chen@applyboard.com>
2023-12-05 02:34:03 -0500

    p5: generate new ranges each step

--- p5b.py
+++ p5b.py
@@ -1,43 +1,42 @@
+from typing import Tuple
 from typing_extensions import TypedDict
 from utils import get_input
-from pprint import pprint
-import itertools
 
 data = get_input(5)
-data = get_input("5e")
-# data[0] = "seeds: " + " ".join(str(x) for x in range(100))
-seed_ranges = [(i, 1) for i in range(0, 100)]
 
 Permutation = TypedDict("permutation", {"min": int, "max": int, "offset": int})
-mappings: list[list[Permutation]] = [[]]
-for line_no, line in enumerate(reversed(data)):
+ranges: list[Tuple[int, int]] = []
+mapping: list[Permutation] = []
+for line in data:
     if line.startswith("seeds"):
-        mappings = mappings[:-1]
-        vals = [int(x) for x in line.split(":")[-1].split()]
-        seed_ranges = [(vals[i], vals[i + 1]) for i in range(0, len(vals) - 1, 2)]
-        print("got to seeds", vals)
-        pprint(mappings)
-        for i in itertools.count(0):
-            print("checking", i)
-            val = i
-            for mapping in mappings:
-                for permutation in mapping:
-                    # print(permutation)
-                    if permutation["min"] <= val <= permutation["max"]:
-                        val += permutation["offset"]
-                        break
-            # print(i, val, seed_ranges)
-            for min, r in seed_ranges:
-                if min <= val <= min + r - 1:
-                    print(i, val, min, r)
-                    exit(0)
-
+        raw = [int(x) for x in line.split(":")[-1].split()]
+        ranges = [(raw[i], raw[i + 1]) for i in range(0, len(raw) - 1, 2)]
     elif line.endswith("map:"):
-        mappings.append([])
-    elif not line:
-        pass
-    elif not line.endswith("map:"):
+        mapping = []
+    elif line:
         dest_min, src_min, r = [int(x) for x in line.split()]
-        dest_max = dest_min + r - 1
-        offset = src_min - dest_min
-        mappings[-1].append({"min": dest_min, "max": dest_max, "offset": offset})
+        src_max = src_min + r - 1
+        offset = dest_min - src_min
+        mapping.append({"min": src_min, "max": src_max, "offset": offset})
+    elif not line and mapping:
+        next_ranges = []
+        while ranges:
+            r = ranges.pop()
+            r_min, r_range = r
+            r_max = r_min + r_range - 1
+            for permutation in mapping:
+                lower = max(r_min, permutation["min"])
+                upper = min(r_max, permutation["max"])
+                if lower <= upper:
+                    next_ranges.append(
+                        (lower + permutation["offset"], upper - lower + 1)
+                    )
+                    if lower > r_min:
+                        ranges.append((r_min, lower - r_min))
+                    if upper < r_max:
+                        ranges.append((upper + 1, r_max - upper))
+                    break
+            else:
+                next_ranges.append(r)
+        ranges = next_ranges
+print(min(r[0] for r in ranges))
