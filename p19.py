e42a24d8fbc50d20e2b54cd9ff18bd913a093552
Michael Chen <michael.chen@applyboard.com>
2023-12-19 10:48:50 -0500

    p19

---
+++ p19.py
@@ -0,0 +1,43 @@
+import sys
+from collections import defaultdict
+
+file = open(sys.argv[1]).read().strip()
+rules, parts = file.split("\n\n")
+
+rule_map = defaultdict(list)
+for line in rules.split("\n"):
+    rule_name, rest = line.split("{")
+    rest = rest[:-1]
+    for rule in rest.split(","):
+        if "<" in rule:
+            a, b = rule.split("<")
+            num, dest = b.split(":")
+            func = lambda x, dest=dest, a=a, num=num: dest if x[a] < int(num) else None
+            rule_map[rule_name].append({"f": func, "desc": rule})
+        elif ">" in rule:
+            a, b = rule.split(">")
+            num, dest = b.split(":")
+            func = lambda x, dest=dest, a=a, num=num: dest if x[a] > int(num) else None
+            rule_map[rule_name].append({"f": func, "desc": rule})
+        else:
+            func = lambda _, rule=rule: rule
+            rule_map[rule_name].append({"f": func, "desc": rule})
+
+total = 0
+for part in parts.split("\n"):
+    part = part[1:-1]
+    map = {}
+    for var in part.split(","):
+        a, b = var.split("=")
+        map[a] = int(b)
+    cur = "in"
+    while cur not in "AR":
+        rules = rule_map[cur]
+        for rule in rules:
+            next = rule["f"](map)
+            if next:
+                cur = next
+                break
+    if cur == "A":
+        total += sum(map.values())
+print(total)
---
+++ p19b.py
@@ -0,0 +1,101 @@
+import sys
+import itertools
+from collections import defaultdict
+from typing import DefaultDict
+from math import prod
+
+file = open(sys.argv[1]).read().strip()
+rules, parts = file.split("\n\n")
+
+rule_map: DefaultDict[str, list[dict]] = defaultdict(list)
+naive_map = defaultdict(list)
+for line in rules.split("\n"):
+    rule_name, rest = line.split("{")
+    rest = rest[:-1]
+    for idx, rule in enumerate(rest.split(",")):
+        if "<" in rule:
+            a, b = rule.split("<")
+            num, dest = b.split(":")
+            rule_map[rule_name].append(
+                {
+                    "c": a,
+                    "op": "<",
+                    "num": int(num),
+                    "desc": rule,
+                    "dest": f"A{idx}" if dest == "A" else dest,
+                    "idx": idx,
+                }
+            )
+            naive_map[rule_name].append(dest)
+        elif ">" in rule:
+            a, b = rule.split(">")
+            num, dest = b.split(":")
+            rule_map[rule_name].append(
+                {
+                    "c": a,
+                    "op": ">",
+                    "num": int(num),
+                    "desc": rule,
+                    "dest": f"A{idx}" if dest == "A" else dest,
+                    "idx": idx,
+                }
+            )
+            naive_map[rule_name].append(dest)
+        else:
+            rule_map[rule_name].append(
+                {"c": "", "op": "", "num": 0, "desc": rule, "dest": rule, "idx": idx}
+            )
+            naive_map[rule_name].append(rule)
+
+paths = []
+came_from = {}
+nodes = ["in"]
+while nodes:
+    node = nodes.pop()
+    ns = naive_map[node]
+    for i, n in enumerate(ns):
+        came_from[n] = node
+        if n == "A":
+            path = [f"A{i}"]
+            cur = node
+            while True:
+                path.append(cur)
+                if cur == "in":
+                    break
+                cur = came_from[cur]
+            paths.append(list(reversed(path)))
+        elif n == "R":
+            pass
+        else:
+            nodes.append(n)
+
+
+total = 0
+for path in paths:
+    restrictions = {
+        "x": {"min": 1, "max": 4001},
+        "m": {"min": 1, "max": 4001},
+        "a": {"min": 1, "max": 4001},
+        "s": {"min": 1, "max": 4001},
+    }
+    for n1, n2 in itertools.pairwise(path):
+        rules = rule_map[n1]
+        for rule in rules:
+            c = rule["c"]
+            op = rule["op"]
+            num = rule["num"]
+            if rule["dest"] == n2:
+                if op == ">":
+                    restrictions[c]["min"] = max(restrictions[c]["min"], num + 1)
+                elif op == "<":
+                    restrictions[c]["max"] = min(restrictions[c]["max"], num)
+                break
+            else:
+                if op == ">":
+                    restrictions[c]["max"] = min(restrictions[c]["max"], num + 1)
+                elif op == "<":
+                    restrictions[c]["min"] = max(restrictions[c]["min"], num)
+
+    ranges = [c["max"] - c["min"] for c in restrictions.values()]
+    total += prod(ranges)
+print(total)
